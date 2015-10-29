d2fd0b7b039acb78fef55b704e852c5ea53d6497
biud436 <biud436@gmail.com>
2015-10-29 23:38:05 +0900

    .

--- exam/js/plugins.js
+++ exam/js/plugins.js
@@ -4,5 +4,5 @@ var $plugins =
 [
 {"name":"RS_HUD","status":true,"description":"체력, 마력, 경험치를 표시하는 플러그인입니다.","parameters":{"가로":"317","세로":"101","간격":"0","가장자리옵션":"true","블러처리":"true"}},
 {"name":"Scene_RotateTitle","status":true,"description":"원형 회전 타이틀 메뉴","parameters":{"간격":"80","메뉴 사이즈":"3","각속도":"120.0","텍스트 테두리 색상":"#6799FF","텍스트의 톤":"0xD9E5FF","게임 시작":"Game Start","게임 계속":"Game Continue","게임 옵션":"Game Options"}},
-{"name":"TDDP_PreloadManager","status":true,"description":"1.0.0 Preload resources on scene/map load as well as game boot for a smoother gameplay experience.","parameters":{"Preload System SFX":"false","Print Debug to Console":"false"}}
+{"name":"TDDP_PreloadManager","status":false,"description":"1.0.0 Preload resources on scene/map load as well as game boot for a smoother gameplay experience.","parameters":{"Preload System SFX":"false","Print Debug to Console":"false"}}
 ];
