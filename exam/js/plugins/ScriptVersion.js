e0d51d5ab7311ead723488e16f08b0e138bfa169
biud436 <biud436@gmail.com>
2015-10-30 15:59:38 +0900

    .

--- exam/js/plugins/ScriptVersion.js
+++ exam/js/plugins/ScriptVersion.js
@@ -9,9 +9,11 @@
 
 (function() {
  
-  var parameters = $plugins.filter( function(i) { if(i.name === "RS_HUD") return true; }.bind(this))[0].parameters;
-  var scriptVersion = "?version=" + (String(parameters["Version"] || '201510292'));
-   
+  var metaData = document.getElementsByName("scripts-version")[0];
+  var scriptVersion = metaData.content;
+  // var parameters = $plugins.filter( function(i) { if(i.name === "RS_HUD") return true; }.bind(this))[0].parameters;
+  // var scriptVersion = "?version=" + (String(parameters["Version"] || '201510292'));
+  
   Utils.canReadGameFiles = function() {
       var scripts = document.getElementsByTagName('script');
       var lastScript = scripts[scripts.length - 1];
