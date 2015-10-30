0c4f014998d9a7c52e2e2fd79d6c49978230f766
biud436 <biud436@gmail.com>
2015-10-30 15:45:06 +0900

    .

--- exam/js/plugins.js
+++ exam/js/plugins.js
@@ -6,5 +6,5 @@ var $plugins =
 {"name":"Scene_RotateTitle","status":true,"description":"원형 회전 타이틀 메뉴","parameters":{"간격":"80","메뉴 사이즈":"3","각속도":"120.0","텍스트 테두리 색상":"#6799FF","텍스트의 톤":"0xD9E5FF","게임 시작":"Game Start","게임 계속":"Game Continue","게임 옵션":"Game Options"}},
 {"name":"TDDP_PreloadManager","status":false,"description":"1.0.0 Preload resources on scene/map load as well as game boot for a smoother gameplay experience.","parameters":{"Preload System SFX":"false","Print Debug to Console":"false"}},
 {"name":"YEP_EquipCore","status":true,"description":"Allows for the equipment system to be more flexible to\nallow for unique equipment slots per class.","parameters":{"---General---":"","Text Align":"center","Finish Command":"Finish","Remove Text":"Remove","Remove Icon":"16","Empty Text":"<Empty>","Empty Icon":"16","---Rules---":"","Non-Removable Types":"1","Non-Optimized Types":"5"}},
-{"name":"ScriptVersion","status":true,"description":"ScriptVersion","parameters":{"Version":"20151030"}}
+{"name":"ScriptVersion","status":true,"description":"ScriptVersion","parameters":{"Version":"2015103003"}}
 ];
