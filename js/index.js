4e7b912ef60713ee11c8e22ae5d90e9cf6c5ee5e
biud436 <biud436@gmail.com>
2021-01-27 22:15:13 +0900

    더보기 패턴 추가

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ class App {
     create() {
         this.createEmptyCanvas();
         this.createContainer();
+        this.createMoreTextButton();
     }
 
     createEmptyCanvas() {
@@ -150,9 +151,46 @@ class App {
             });
 
         });
+    }
 
-        
-        
+    createMoreTextButton() {
+
+        const items = Array.from(document.querySelectorAll(".card-description"));
+
+        items.forEach(div => {
+            const moreButton = new MoreButton(div);
+            moreButton.connect();
+        })
+    }
+}
+
+class MoreButton {
+    constructor(elem) {
+        /**
+         * @type {HTMLDivElement}
+         */
+        this._elem = elem;
+    }
+
+    toggle() {
+        // return if the element is not exists?
+        if(!this._elem) {
+            return;
+        }
+
+        // remove it if the element is contained?
+        if(this._elem.classList.contains("active")) {
+            this._elem.classList.remove("active");
+        } else {
+            this._elem.classList.add("active");
+        }
+    }
+
+    connect() {
+        if(!this._elem) {
+            return false;
+        }
+        this._elem.onclick = () => this.toggle();
     }
 }
 
