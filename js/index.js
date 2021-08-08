a8952009697b34437303ba34807da3545e7ecf70
biud436 <biud436@gmail.com>
2021-08-08 21:21:03 +0900

    test nuxtjs

--- js/index.js
+++ js/index.js
@@ -1,54 +1,39 @@
-import {MoreButton} from "./MoreButton.js";
-import {CanvasManager} from "./CanvasManager.js";
-import {SkillComponent} from "./SkillComponent.js";
+import { MoreButton } from './MoreButton.js';
+import { CanvasManager } from './CanvasManager.js';
 
 /**
  * @author 어진석
  */
 class App {
-    constructor() {
+  constructor() {}
+
+  create() {
+    // 캔버스 생성
+    this.createCanvas();
+
+    // 더보기 목록 동적 생성
+    this.createMoreTextButton();
+  }
+
+  createCanvas() {
+    try {
+      this._canvas = new CanvasManager();
+    } catch (e) {
+      console.warn(e);
     }
+  }
 
-    create() {
+  createMoreTextButton() {
+    const items = Array.from(document.querySelectorAll('.card-description'));
 
-        // 캔버스 생성
-        this.createCanvas();
-
-        // 더보기 목록 동적 생성
-        this.createMoreTextButton();
-
-        // 동적 스킬 목록 생성
-        this.createSkillList();
-    }
-
-    createCanvas() {
-        try {
-            this._canvas = new CanvasManager();
-        } catch(e) {
-            console.warn(e);
-        }
-    }
-
-    createMoreTextButton() {
-
-        const items = Array.from(document.querySelectorAll(".card-description"));
-
-        items.forEach(div => {
-            const moreButton = new MoreButton(div);
-            moreButton.connect();
-        })
-    }
-
-    /**
-     * Creates a skill list in my portfolio using Vue.js
-     */
-    createSkillList() {
-        const skills = new SkillComponent();
-        const container = new Vue({
-            el: '#skill-list'
-        })
-    }
+    items.forEach((div) => {
+      const moreButton = new MoreButton(div);
+      moreButton.connect();
+    });
+  }
 }
 
-const app = new App();
-app.create();
+window.addEventListener('load', () => {
+  const app = new App();
+  app.create();
+});
--- js/typeWriter.js
+++ js/typeWriter.js
@@ -2,30 +2,26 @@
  * @author 어진석
  */
 
-(function() {
+(function () {
+  class TypeWriterApp {
+    constructor() {
+      const app = document.querySelector(
+        '.masthead > .container > .masthead-subheading'
+      );
 
-    class TypeWriterApp {
-        constructor() {
-            
-            const app = document.querySelector(".masthead > .container > .masthead-subheading");
+      this._title = new Typewriter(app, {
+        loop: true,
+      });
 
-            this._title = new Typewriter(app, {
-                loop: true
-            });            
-
-            this.run();
-        }
-
-        run() {
-            
-            this._title.typeString("Eo JinSeok's Portfolio.")
-                .pauseFor(2500)
-                .start();
-
-        }
+      this.run();
     }
 
+    run() {
+      this._title.typeString("Eo JinSeok's Portfolio.").pauseFor(2500).start();
+    }
+  }
+
+  window.addEventListener('load', () => {
     window.app = new TypeWriterApp();
-
-
+  });
 })();
